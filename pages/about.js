568bc925c2d8d89a0a35dab64779a43690d0e8a3
Hoang Lam <hoanglam271096@gmail.com>
2021-11-02 18:57:04 +0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Laboris mollit eu nulla tempor velit aliqua irure excepteur
+                    voluptate.
+                </p>
+                <p>
+                    Laboris mollit eu nulla tempor velit aliqua irure excepteur
+                    voluptate.
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default About;
